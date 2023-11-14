56b37105fc0ed524f648d70ed798558df0fe83aa
AndreiPalonpon <150495745+AndreiPalonpon@users.noreply.github.com>
2023-11-14 08:43:03 +0000

    Revised Warehouses further and added registerWarehouse functionality.

--- src/java/warehouseMgmt/Warehouses.java
+++ src/java/warehouseMgmt/Warehouses.java
@@ -15,22 +15,47 @@ public class Warehouses {
     public int maxBinNumber;
 
     //Constructor
-    public Warehouses(int warehouseID, String city, String phoneNumber, String firstAddress,
-                     String state, String country, String postalCode, boolean isFunctional,
-                     int maxRow, int maxBay, int maxLevel, int maxBinNumber) {
-                        this.warehouseID = warehouseID;
-                        this.city = city;
-                        this.phoneNumber = phoneNumber;
-                        this.firstAddress = firstAddress;
-                        this.state = state;
-                        this.country = country;
-                        this.postalCode = postalCode;
-                        this.isFunctional = isFunctional;
-                        this.maxRow = maxRow;
-                        this.maxBay = maxBay;    
-                        this.maxLevel = maxLevel;
-                        this.maxBinNumber = maxBinNumber;
-                     }
+    public Warehouses() {}
+
+    public int registerWarehouse() {
+        try {
+            //1. Connect to database.
+            Connection conn;
+            conn = DriverManager.getConnection("") //Place the path.
+            System.out.println("Connection Successful")
+
+            //2. To get the next warehouseID.
+            //2.1 Get the next warehosueID.
+            PreparedStatement pstmt = conn.prepareStatement("") //Place the SQL statement.
+            ResultSet rst = pstmt.executeQuery();
+            while (rst.next()) {
+                productID = rst.getInt("newID");
+            }
+
+            //2.2 Save the new warehouse.
+            pstmt = conn.prepareStatement("") //Place INSERT INTO statement.
+            pstmt.setInt(1, warehouseID);
+            pstmt.setString(2, city);
+            pstmt.setString(3, phoneNumber);
+            pstmt.setString(4, firstAddress);
+            pstmt.setString(5, state);
+            pstmt.setString(6, country);
+            pstmt.setString(7, postalCode);
+            pstmt.setBoolean(8, isFunctional);
+            pstmt.setInt(9, maxRow);
+            pstmt.setInt(10, maxBay);
+            pstmt.setInt(11, maxLevel);
+            pstmt.setInt(12, maxBinNumber);
+
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
 
     //Testing
     public static void main() {
