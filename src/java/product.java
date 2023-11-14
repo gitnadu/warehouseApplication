d77ea639c2d4da06c6e01e2891a520e1ec6152ac
AndreiPalonpon <150495745+AndreiPalonpon@users.noreply.github.com>
2023-11-14 06:52:02 +0000

    Developed a class for product but it is incomplete.

---
+++ src/java/product.java
@@ -0,0 +1,63 @@
+package productManagement;
+
+import java.util.*;
+
+//Enums
+enum condEnum { //condEnum = condition enum
+    BAD,
+    GOOD,
+    DISPOSED;
+}
+
+enum measureEnum {
+    EACH,
+    GROUP;
+}
+
+enum statusEnum {
+    STORED,
+    DISPOSED;
+}
+
+public class product {
+    
+    //Fields
+    public int productID; //public
+    public int warehouseID;
+    public Date dateReceived;
+    public int productLineID;
+    public condEnum productCond; //productCond = product condition
+    public String reason;
+    public double stockPrice;
+    public int supplierID;
+    public measureEnum unitMeasure; //enum
+    public boolean isReceivedFromWarehouse;
+    public statusEnum status; //enum 
+
+    //Default constructor
+
+    //Constructor
+    public product(int productID, int warehouseID, Date dateReceived, int productLineID,
+                   condEnum productCond, String reason, double stockPrice, int supplierID,
+                   measureEnum unitMeasure, boolean isReceivedFromWarehouse, statusEnum status) 
+                   {
+                    this.productID = productID;
+                    this.warehouseID = warehouseID;
+                    this.dateReceived = dateReceived;
+                    this.productLineID = productLineID;
+                    this.productCond = productCond;
+                    this.reason = reason;
+                    this.stockPrice = stockPrice;
+                    this.supplierID = supplierID;
+                    this.unitMeasure = unitMeasure;
+                    this.isReceivedFromWarehouse = isReceivedFromWarehouse;
+                    this.status = status;
+    }
+    
+    //Setters and getters
+
+    //Testing
+    public static void main() {
+
+    }
+}
