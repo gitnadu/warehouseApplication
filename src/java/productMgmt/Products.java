d955885ac74d1fd4ddbc9744f81aeb87270fe3a6
AndreiPalonpon <150495745+AndreiPalonpon@users.noreply.github.com>
2023-11-14 08:28:25 +0000

    Added registerProduct functionality but not completed.

--- src/java/productMgmt/Products.java
+++ src/java/productMgmt/Products.java
@@ -1,6 +1,7 @@
 package productMgmt;
 
 import java.util.*;
+import java.sql.*;
 
 //Enums
 enum condEnum { //condEnum = condition enum
@@ -49,6 +50,45 @@ public class Product {
 
     //Constructor
     public Products() {}
+
+    public int registerProduct() {
+        try {
+            //1. Connect to database.
+            Connection conn;
+            conn = DriverManager.getConnection("") //Place the path.
+            System.out.println("Connection Successful")
+
+            //2. To get the next productID.
+            //2.1 Get the next productID.
+            PreparedStatement pstmt = conn.prepareStatement("") //Place the SQL statement.
+            ResultSet rst = pstmt.executeQuery();
+            while (rst.next()) {
+                productID = rst.getInt("newID");
+            }
+
+            //2.2 Save the new product
+            pstmt = conn.prepareStatement("") //Place INSERT INTO statement.
+            pstmt.setInt(1, productID);
+            pstmt.setInt(2, warehouseID);
+            pstmt.setDate(3, dateReceived);
+            pstmt.setInt(4, productLineID);
+            pstmt.setString(5, productCond); //enum
+            pstmt.setString(6, reason);
+            pstmt.setDouble(7, stockPrice);
+            pstmt.setInt(8, supplierID);
+            pstmt.setString(9, unitMeasure); //enum
+            pstmt.setBoolean(10, isReceivedFromWarehouse); //boolean
+            pstmt.setString(11, status); //enum
+
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
 
     //Testing
     public static void main() {
